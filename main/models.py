a2579ff4b35286a4ab0d20682eb1fe17e929c363
Richard Hertzog <richard.hertzog@gmail.com>
2017-05-23 23:17:53 -0700

    model created and migrated

--- main/models.py
+++ main/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# id, dropoff, pickup, mechanic, repair_type
+class Service(models.Model):
+    id = models.IntegerField(primary_key=True)
+    dropoff = models.CharField(max_length=100)
+    pickup = models.CharField(max_length=100)
+    mechanic = models.CharField(max_length=50)
+    repair_type = models.CharField(max_length=10)
